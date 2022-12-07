66e29c00c01570b0b477f30bda210ee47ae31ce3
jorgegoco <jorgegoco70@gmail.com>
2022-12-07 15:04:42 +0100

    Add Caculator styling

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -8,25 +8,26 @@ class Calculator extends Component {
   render() {
     return (
       <div className="calculator-container">
-        <div className="ouput">0</div>
-        <button type="button">AC</button>
-        <button type="button">+/-</button>
-        <button type="button">%</button>
-        <button type="button">+</button>
-        <button type="button">7</button>
-        <button type="button">8</button>
-        <button type="button">9</button>
-        <button type="button">*</button>
-        <button type="button">4</button>
-        <button type="button">5</button>
-        <button type="button">6</button>
-        <button type="button">-</button>
-        <button type="button">1</button>
-        <button type="button">2</button>
-        <button type="button">3</button>
-        <button type="button">+</button>
-        <button type="button">0</button>
-        <button type="button">=</button>
+        <div className="output">0</div>
+        <button className="ac" type="button">AC</button>
+        <button className="plus-minus" type="button">+/-</button>
+        <button className="percentage" type="button">%</button>
+        <button className="division" type="button">/</button>
+        <button className="seven" type="button">7</button>
+        <button className="eight" type="button">8</button>
+        <button className="nine" type="button">9</button>
+        <button className="product" type="button">*</button>
+        <button className="four" type="button">4</button>
+        <button className="five" type="button">5</button>
+        <button className="six" type="button">6</button>
+        <button className="minus" type="button">-</button>
+        <button className="one" type="button">1</button>
+        <button className="two" type="button">2</button>
+        <button className="three" type="button">3</button>
+        <button className="plus" type="button">+</button>
+        <button className="zero" type="button">0</button>
+        <button className="dot" type="button">.</button>
+        <button className="equal" type="button">=</button>
       </div>
     );
   }
