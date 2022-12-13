77ade2ba9e585c8a084dbb862ada250726d42971
jorgegoco <jorgegoco70@gmail.com>
2022-12-13 12:04:47 +0100

    Setup basic structure of pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
 import Calculator from './Components/Calculator';
+import Home from './Components/Home';
+import Quote from './Components/Quote';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const App = () => (
+  <>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/calculator">Calculator</Link></li>
+        <li><Link to="/quote">Quote</Link></li>
+      </ul>
+    </nav>
 
-  render() {
-    return (
-      <Calculator />
-    );
-  }
-}
-
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/calculator" element={<Calculator />} />
+      <Route path="/quote" element={<Quote />} />
+    </Routes>
+  </>
+);
 export default App;
