792545d032a57385c9a3066eeb9a75041de69a17
jorgegoco <jorgegoco70@gmail.com>
2022-12-14 10:57:07 +0100

    Fix Router issues

--- src/App.js
+++ src/App.js
@@ -10,17 +10,19 @@ const App = () => (
       <h1>Math Magicians</h1>
       <nav>
         <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/calculator">Calculator</Link></li>
-          <li><Link to="/quote">Quote</Link></li>
+          <li><Link to="/math-magicians">Home</Link></li>
+          <li><Link to="/math-magicians/calculator">Calculator</Link></li>
+          <li><Link to="/math-magicians/quote">Quote</Link></li>
         </ul>
       </nav>
     </header>
 
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/calculator" element={<Calculator />} />
-      <Route path="/quote" element={<Quote />} />
+      <Route index element={<Home />} />
+      <Route path="/math-magicians" element={<Home />} />
+      <Route index element={<Home />} />
+      <Route path="/math-magicians/calculator" element={<Calculator />} />
+      <Route path="/math-magicians/quote" element={<Quote />} />
     </Routes>
   </>
 );
